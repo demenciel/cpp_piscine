fb8626d8bc2e136931b021b3e9f57ee159a764fb
Alex Couture <acouture@Alexs-MacBook-Air.local>
2023-11-11 13:06:31 -0500

    ok

--- CPP_09Home/ex02/src/main.cpp
+++ CPP_09Home/ex02/src/main.cpp
@@ -9,11 +9,19 @@ int main(int ac, char **av)
     return 1;
   }
 
-  clock_t startVector, endVector;
-  double timeSortVector;
   PMergeMe pm;
   std::string input;
+  for (int i = 1; i < ac; ++i)
+  {
+    input += av[i];
+    if (i < ac - 1)
+    {
+      input += " ";
+    }
+  }
 
+  clock_t startVector, endVector;
+  double vectorTime;
   std::istringstream iss(input);
   try
   {
@@ -28,25 +36,11 @@ int main(int ac, char **av)
     std::cerr << e.what() << std::endl;
     return 1;
   }
-
-  size_t numberOfAv = 0;
   std::cout << "Before: ";
-  for (int i = 1; i < ac; ++i)
-  {
-    input += av[i];
-    if (i < ac - 1)
-    {
-      input += " ";
-      numberOfAv++;
-      std::cout << input;
-    }
-  }
-  std::cout << std::endl;
-  std::cout << "After: ";
-  pm.printMergedVector();
-
-  timeSortVector = ((double)(endVector - startVector)) / CLOCKS_PER_SEC;
-  std::cout << "Time to process a range of " << numberOfAv << " elements with std::vector : " << timeSortVector << std::endl;
+  std::cout << input << std::endl;
+  std::cout << "After: "; pm.printMergedVector();
+  vectorTime = ((double) (endVector - startVector)) / CLOCKS_PER_SEC;
+  std::cout << "Time to process a range of " << input.size() << " elements with std::vector : " << vectorTime << std::endl;
 
   return 0;
 }
